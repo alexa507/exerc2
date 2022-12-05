bbbfb16e0e20588b7c9efb7fdf2a9ff168b08bb6
alexa507 <alexandra.ionila@gmail.com>
2022-12-05 18:51:37 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exercitiul2';
+  title = 'Calculator';
+
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
+  selectedOperation: any;
+  history: any[] = [];
+
+
+  operations: any[] = [
+    { name: "+", code: "+" },
+    { name: "-", code: "-" },
+    { name: "*", code: "*" },
+    { name: "/", code: "/" }
+  ];
+
+  calculate(){
+    const currentDate = new Date();
+    if (this.selectedOperation.code == "+"){
+      this.result = this.num1 + this.num2;
+      this.history.push({
+        time:currentDate, request:this.num1.toString()+"+"+this.num2.toString(), result:this.result
+      })
+    }else if(this.selectedOperation.code == "-"){
+      this.result = this.num1 - this.num2;
+      this.history.push({
+        time:currentDate, request:this.num1.toString()+"-"+this.num2.toString(), result:this.result
+      })
+    }else if(this.selectedOperation.code == "*"){
+      this.result = this.num1 * this.num2;
+      this.history.push({
+        time:currentDate, request:this.num1.toString()+"*"+this.num2.toString(), result:this.result
+      })
+    }else {
+      this.result = this.num1 / this.num2;
+      this.history.push({
+        time:currentDate, request:this.num1.toString()+"/"+this.num2.toString(), result:this.result
+      })
+    }
+  }
+
+  delete(calculation: any){
+    const index: number = this.history.indexOf(calculation);
+    if (index !== -1) {
+        this.history.splice(index, 1);
+    } 
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DropdownModule,
+    ButtonModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
